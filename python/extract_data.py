6e9fe5c006116ab48547b195a2c73023a85de3cc
Katsuya-Ishiyama <ishiyama.katsuya@gmail.com>
2021-12-07 01:52:31 +0900

    extract_dataにログを埋め込んだ

--- python/extract_data.py
+++ python/extract_data.py
@@ -1,8 +1,15 @@
 from datetime import datetime
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 
 class ExtractError(Exception):
     pass
+
+
+def extract_line_identifier(line: str):
+    return line[:15].replace(" ", "")
 
 
 def extract_datetime(line: str) -> datetime:
@@ -13,24 +20,34 @@ def extract_datetime(line: str) -> datetime:
 
     minute = int(line[11:13])
     if (minute < 0) or (59 < minute):
+        line_id = extract_line_identifier(line)
+        logger.warning("minute was out of range: %d, %s", minute, line_id)
         raise ExtractError
 
     try:
         second = int(line[13:15])
     except ValueError:
         second = 0
+        line_id = extract_line_identifier(line)
+        logger.warning('second "%s" was converted into %d, %s', line[13:15], second, line_id)
     except Exception as e:
         raise e
     return datetime(year, month, day, hour, minute, second)
 
 
-def convert_latitude_longitude_deg(degree: str, minute: str, second: str) -> float:
-    deg_degree = int(degree)
-    deg_minute = int(minute) / 60
+def convert_latitude_longitude_deg(line: str, degree: int, minute: int, second: int) -> float:
+    _degree = line[degree:minute]
+    _minute = line[minute:second]
+    _second = line[second:(second+2)]
+
+    deg_degree = int(_degree)
+    deg_minute = int(_minute) / 60
     try:
-        deg_second = int(second) / 3600
+        deg_second = int(_second) / 3600
     except ValueError:
         deg_second = 0.0
+        line_id = extract_line_identifier(line)
+        logger.warning('second "%s" was converted into %d, %s', second, deg_second, line_id)
     except Exception as e:
         raise e
 
@@ -43,6 +60,8 @@ def extract_depth(line: str) -> float:
         _decimal = int(line[47:49]) / 100
     except ValueError:
         _decimal = 0
+        line_id = extract_line_identifier(line)
+        logger.warning('decimal "%s" was converted into %d, %s', line[47:49], _decimal, line_id)
     except Exception as e:
         raise e
     return _int + _decimal
@@ -64,14 +83,16 @@ def extract_epicenter(_line: str):
 
     extracted["time"] = extract_datetime(_line)
     extracted["latitude"] = convert_latitude_longitude_deg(
-        degree=_line[21:24],
-        minute=_line[24:26],
-        second=_line[26:28]
+        line=_line,
+        degree=21,
+        minute=24,
+        second=26
     )
     extracted["longitude"] = convert_latitude_longitude_deg(
-        degree=_line[32:36],
-        minute=_line[36:38],
-        second=_line[38:40]
+        line=_line,
+        degree=32,
+        minute=36,
+        second=38
     )
     extracted["depth"] = extract_depth(_line)
     extracted["magnitude1"] = extract_magnitude(_line[52:54])
