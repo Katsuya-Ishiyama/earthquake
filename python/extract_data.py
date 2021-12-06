340b124f1824f40bbc7267e0232c6fb339f001fa
Katsuya-Ishiyama <ishiyama.katsuya@gmail.com>
2021-12-07 00:45:11 +0900

    A19190101121537のテストを通過

--- python/extract_data.py
+++ python/extract_data.py
@@ -14,16 +14,35 @@ def extract_datetime(line: str) -> datetime:
 def convert_latitude_longitude_deg(degree: str, minute: str, second: str) -> float:
     deg_degree = int(degree)
     deg_minute = int(minute) / 60
-    deg_second = int(second) / 3600
+    try:
+        deg_second = int(second) / 3600
+    except ValueError:
+        deg_second = 0.0
+    except Exception as e:
+        raise e
+
     return deg_degree + deg_minute + deg_second
 
 
 def extract_depth(line: str) -> float:
-    return int(line[44:47]) + int(line[47:49]) / 100
+    _int = int(line[44:47])
+    try:
+        _decimal = int(line[47:49]) / 100
+    except ValueError:
+        _decimal = 0
+    except Exception as e:
+        raise e
+    return _int + _decimal
 
 
 def extract_magnitude(magnitude_str: str) -> float:
-    return float(magnitude_str.strip()) / 10.0
+    try:
+        magnitude = float(magnitude_str.strip()) / 10.0
+    except ValueError:
+        magnitude = None
+    except Exception as e:
+        raise e
+    return magnitude
 
 
 def extract_epicenter(line: str):
@@ -42,17 +61,17 @@ def extract_epicenter(line: str):
     )
     extracted["depth"] = extract_depth(line)
     extracted["magnitude1"] = extract_magnitude(line[52:54])
-    extracted["magnitude1_type"] = line[54].strip()
+    extracted["magnitude1_type"] = line[54] if line[54] != " " else None
     extracted["magnitude2"] = extract_magnitude(line[55:57])
-    extracted["magnitude2_type"] = line[57].strip()
-    extracted["travel_time_type"] = int(line[58].strip())
-    extracted["epicenter_evaluation"] = int(line[59].strip())
-    extracted["epicenter_information"] = int(line[60].strip())
+    extracted["magnitude2_type"] = line[57] if line[57] != " " else None
+    extracted["travel_time_type"] = int(line[58]) if line[58] != " " else None
+    extracted["epicenter_evaluation"] = int(line[59]) if line[59] != " " else None
+    extracted["epicenter_information"] = int(line[60]) if line[60] != " " else None
     extracted["max_seismic"] = int(line[61].strip())
-    extracted["damage"] = int(line[62].strip())
-    extracted["tsunami"] = int(line[63].strip())
-    extracted["region1"] = int(line[64].strip())
-    extracted["region2"] = int(line[65:68].strip())
+    extracted["damage"] = int(line[62]) if line[62] != " " else None
+    extracted["tsunami"] = int(line[63]) if line[63] != " " else None
+    extracted["region1"] = int(line[64]) if line[64] != " " else None
+    extracted["region2"] = int(line[65:68].strip()) if line[65:68] != "   " else None
     extracted["epicenter_name"] = line[68:90].split(" ")[0]
     extracted["observation_point"] = int(line[-6:-1].strip())
     extracted["epicenter_decision"] = line[-1].strip()
