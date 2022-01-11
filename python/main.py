d0d8394b05466fec88da8d28b4ed697d9562c26f
Katsuya-Ishiyama <ishiyama.katsuya@gmail.com>
2022-01-11 23:36:00 +0900

    データをparquetで出力した

--- python/main.py
+++ python/main.py
@@ -2,9 +2,40 @@ import csv
 import logging
 from datetime import datetime
 from pathlib import Path
+import pandas as pd
 import extract_data as ex
 
 logger = logging.getLogger(__name__)
+
+
+def read_dat_as_DataFrame(input_filepath):
+    logger.info(f"reading {input_filepath}")
+    converted_count = 0
+    start_ts = datetime.now()
+    records = []
+    with input_filepath.open("r") as fin:
+        for line in fin:
+            if not line.startswith("A"):
+                continue
+
+            try:
+                epicenter = ex.extract_epicenter(line)
+            except ex.ExtractError as e:
+                logger.warning("skipped due to ExtractError: %s", line)
+                continue
+            except Exception as e:
+                logger.error("error line: %s", line)
+                raise e
+
+            records.append(epicenter)
+            converted_count += 1
+
+    df = pd.DataFrame.from_records(records)
+    end_ts = datetime.now()
+    elapsed_time = end_ts - start_ts
+    logger.info(f"finish reading from {input_filepath}, shape: {df.shape}, time {elapsed_time}"),
+
+    return df
 
 
 def convert_dat_to_tsv(input_filepath, output_filepath):
@@ -37,15 +68,12 @@ def convert_dat_to_tsv(input_filepath, output_filepath):
 
 def main():
     input_dir = Path("../data/utf-8")
-    output_dir = Path("../data/tsv")
 
-    for input_filepath in input_dir.glob("i*.dat"):
-        print(input_filepath)
-        output_filepath = output_dir / f"{input_filepath.stem[1:]}.tsv"
-        convert_dat_to_tsv(
-            input_filepath=input_filepath,
-            output_filepath=output_filepath
-        )
+    df_list = [read_dat_as_DataFrame(path) for path in input_dir.glob("i*.dat")]
+    df_all = pd.concat(df_list)
+    df_all["year"] = df_all.time.map(lambda x: x.year)
+    df_all.sort_values(by="id", inplace=True)
+    df_all.to_parquet("../data/parquet/epicenters", compression="gzip", partition_cols="year")
 
 
 if __name__ == "__main__":
